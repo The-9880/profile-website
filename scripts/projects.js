d6d918161b3324c2d11db18835cb008282fbc0c0
Anant Sharma <anant98@hotmail.com>
2018-10-04 14:55:33 -0400

    More work on the projects page mobile interface.

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,9 +1,9 @@
 $(document).ready(function()
 {
     var styleToggled = false;
-    var stylesheet = document.querySelector("link");
+    var stylesheet = document.querySelectorAll("link")[2];
 
-    $(".styleToggle").click(function(){
+    function toggle() {
         if(!styleToggled)
         {
             // Switch from tiles to list view
@@ -12,6 +12,25 @@ $(document).ready(function()
         } else {
             stylesheet.href = "styles/project-tile-display.css";
             styleToggled = false;
+        }
+    }
+
+    $(".styleToggle").click(() => {
+        toggle();
+    });
+
+    if(document.documentElement.clientWidth <= 650)
+    {
+        $(".styleToggle")[0].checked = true;
+        toggle();
+    }
+
+    $(window).on('resize', function()
+    {
+        if(document.documentElement.clientWidth <= 650 && !styleToggled)
+        {
+            $(".styleToggle")[0].checked = true;
+            toggle();   
         }
     });
 
